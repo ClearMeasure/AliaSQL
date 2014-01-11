da1de72d2fd5e8d2b737b51389cc212355d34626
Eric Coffman <eric@coffman.net>
2014-01-10 20:54:11 -0500

    Update nuspec files to fix problem with & character

--- build_and_package.bat
+++ build_and_package.bat
@@ -1,1 +1,2 @@
 powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "& { Import-Module '.\lib\psakev4\psake.psm1'; Invoke-psake ci -parameters @{%*}; if ($psake.build_success -eq $false) { exit 1 } else { exit 0 } }" 
+pause
