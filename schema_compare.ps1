aa46258a691e10cbcca1bd806f5dc1a8d6fb2591
Eric Coffman <eric@coffman.net>
2014-02-25 17:07:11 -0500

    Update for Powershell 2.0 compatibility

--- schema_compare.ps1
+++ schema_compare.ps1
@@ -6,8 +6,11 @@ $SqlPackage = "C:\Program Files (x86)\Microsoft SQL Server\110\DAC\bin\SqlPackag
 $AliaSQL = "$base_dir\lib\AliaSQL\AliaSQL.exe"
 $databaseName_Original = "$databaseName" + "_Original"
 $databaseScriptsUpdate = "$databaseScripts\Scripts\Update"
-$newScriptName = ((Get-ChildItem $databaseScriptsUpdate -filter "*.sql" | ForEach-Object {[int]$_.Name.Substring(0, 4)} | Sort-Object)[-1] + 1).ToString("0000-") + "$databaseName" + ".sql.temp"
 
+#Powershell 2.0 can't do this so try another way
+#$newScriptName = ((Get-ChildItem $databaseScriptsUpdate -filter "*.sql" | ForEach-Object {[int]$_.Name.Substring(0, 4)} | Sort-Object)[-1] + 1).ToString("0000-") + "$databaseName" + ".sql.temp"
+
+$newScriptName = ([int]((gci $databaseScriptsUpdate -filter "*.sql" | sort CreationTime -desc | select -f 1).Name.Substring(0, 4)) + 1).ToString("0000-") + "$databaseName" + ".sql.temp"
 
 write-host "Building original database..."
 & $AliaSQL Rebuild .\sqlexpress "$databaseName_Original" "$databaseScripts\Scripts"   
@@ -15,7 +18,7 @@ write-host "`n`nGenerating the diff script"
 #generate the needed .dacpac (we'll delete it later)
 & $SqlPackage /a:Extract /ssn:.\SQLEXPRESS /sdn:$databaseName /tf:$databaseScripts$databaseName.dacpac 
 #generate the diff script
-& $SqlPackage /a:Script /op:$databaseScriptsUpdate\$newScriptName /sf:$databaseScripts$databaseName.dacpac /tsn:.\SQLEXPRESS /tdn:"$databaseName_Original"
+& $SqlPackage /a:Script /op:$databaseScriptsUpdate\$newScriptName /p:DropObjectsNotInSource=true /p:BlockOnPossibleDataLoss=false /sf:$databaseScripts$databaseName.dacpac /tsn:.\SQLEXPRESS /tdn:"$databaseName_Original"
    
 write-host "`n`nCleaning up generated script..."
 $scriptLines = Get-Content $databaseScriptsUpdate\$newScriptName
@@ -26,7 +29,7 @@ $skipBlock = $false
 $blocksToSkip = "usd_AppliedDatabaseTestDataScript", "IX_usd_DateApplied"
 $noDiff = $true
 $noDiffLines = "", "GO", "PRINT N'Update complete.';" #these are the only lines left when there are no DB diffs
-foreach($line in $scriptLines)
+foreach($line in $scriptLines) 
 {   
     #don't add anything until we get past the line USE [`$(DatabaseName)]; -- all the previous stuff should be sqlcmd/unncessary junk 
     if ($line -eq "USE [`$(DatabaseName)];")
@@ -45,7 +48,16 @@ foreach($line in $scriptLines)
 
         if ($noDiff)
         {
-            $noDiff = $noDiffLines.Contains($newLine)
+            #Powershell 2.0 can't do this so we revert to the hard way
+            #$noDiff = $noDiffLines.Contains($newLine)
+            $noDiff=$false
+            foreach($cline in $noDiffLines)
+            {
+                if($cline -eq $newLine) 
+                    {
+                    $noDiff = $true
+                    }
+            }
         }
     }
     elseif ($line -eq "GO")
@@ -67,4 +79,3 @@ else
 {
     write-host "Please validate the new script $databaseScriptsUpdate\$newScriptName is correct, then rename to .sql and add to the database project" -foregroundcolor "yellow"
 }
-
