e773358a9068647298734e4872020cd9c4cadf91
Eric Coffman <eric@coffman.net>
2015-02-27 01:25:37 -0500

    Fix for issue with database rebuild when database doesn't exist
    When the database doesn't exist, the connect returns login faiiled then
    connection pooling keeps that bad connection opened. This change tries
    to drop the database and if it fails closes all connections in the pool.

--- source/AliaSQL.Core/Services/Impl/DatabaseDropper.cs
+++ source/AliaSQL.Core/Services/Impl/DatabaseDropper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using AliaSQL.Core.Model;
@@ -24,25 +25,27 @@ namespace AliaSQL.Core.Services.Impl
 
         public void Execute(TaskAttributes taskAttributes, ITaskObserver taskObserver)
         {
-            var version = _queryExecutor.ReadFirstColumnAsStringArray(taskAttributes.ConnectionSettings, "select @@version")[0];
-             taskObserver.Log("Running against: " + version);
-
-            //can't kill connections or enter single user mode in Azure
-            var sql = string.Format("drop database [{0}]", taskAttributes.ConnectionSettings.Database);
-            if (!version.Contains("SQL Azure"))
-            {
-                _connectionDropper.Drop(taskAttributes.ConnectionSettings, taskObserver);
-                sql = string.Format("ALTER DATABASE [{0}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE drop database [{0}]", taskAttributes.ConnectionSettings.Database);
-            }
-
-            string message = string.Format("Dropping database: {0}\n", taskAttributes.ConnectionSettings.Database);
-            taskObserver.Log(message);
             try
             {
+                var version = _queryExecutor.ReadFirstColumnAsStringArray(taskAttributes.ConnectionSettings, "select @@version")[0];
+                 taskObserver.Log("Running against: " + version);
+
+                //can't kill connections or enter single user mode in Azure
+                var sql = string.Format("drop database [{0}]", taskAttributes.ConnectionSettings.Database);
+                if (!version.Contains("SQL Azure"))
+                {
+                    _connectionDropper.Drop(taskAttributes.ConnectionSettings, taskObserver);
+                    sql = string.Format("ALTER DATABASE [{0}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE drop database [{0}]", taskAttributes.ConnectionSettings.Database);
+                }
+
+                string message = string.Format("Dropping database: {0}\n", taskAttributes.ConnectionSettings.Database);
+                taskObserver.Log(message);  
                 _queryExecutor.ExecuteNonQuery(taskAttributes.ConnectionSettings, sql);
             }
             catch (Exception)
             {
+                //if the database doesn't exist just close any open connections and move on
+                SqlConnection.ClearAllPools();
                 taskObserver.Log(string.Format("Database '{0}' could not be dropped.", taskAttributes.ConnectionSettings.Database));
             }
         }
