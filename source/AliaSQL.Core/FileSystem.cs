5f18e7be9dfcc6cdd24dfef776922c868f47ae2d
armannvg <armannvg@yahoo.com>
2015-12-11 22:32:36 +0000

    Add a function that figures out which BOM a file has, so that we can read its content using the detected encoding.

--- source/AliaSQL.Core/FileSystem.cs
+++ source/AliaSQL.Core/FileSystem.cs
@@ -1,6 +1,6 @@
 using System;
 using System.IO;
-
+using System.Text;
 
 namespace AliaSQL.Core
 {
@@ -72,10 +72,37 @@ namespace AliaSQL.Core
         {
             var stream = _streamFactory.ConstructReadFileStream(filename);
 
-            using (var reader = new StreamReader(stream))
+            Encoding encoding = GetEncoding(filename);
+            using (var reader = new StreamReader(stream, encoding))
             {
                 return reader.ReadToEnd();
             }
+        }
+
+        /// <summary>
+        /// Determines a text file's encoding by analyzing its byte order mark (BOM)
+        /// Defaults to ASCII when detection of the text file's endianness fails.
+        /// Function originally from http://stackoverflow.com/questions/3825390/effective-way-to-find-any-files-encoding
+        /// </summary>
+        /// <param name="filename">The text file to analyze.</param>
+        /// <returns>The detected encoding.</returns>
+        private static Encoding GetEncoding(string filename)
+        {
+            // Read the BOM
+            var bom = new byte[4];
+            using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                file.Read(bom, 0, 4);
+            }
+
+            // Analyze the BOM
+            if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76) return Encoding.UTF7;
+            if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) return Encoding.UTF8;
+            if (bom[0] == 0xff && bom[1] == 0xfe) return Encoding.Unicode; //UTF-16LE
+            if (bom[0] == 0xfe && bom[1] == 0xff) return Encoding.BigEndianUnicode; //UTF-16BE
+            if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff) return Encoding.UTF32;
+
+            return Encoding.ASCII;
         }
 
         public StreamReader ReadFileIntoStreamReader(string filename)
