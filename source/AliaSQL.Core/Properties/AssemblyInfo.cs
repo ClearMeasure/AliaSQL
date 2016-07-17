e65043a720d0ed93a3bd7103df7b992085a1da30
Mark Gidman <cravelight@gmail.com>
2016-07-16 19:59:34 -0500

    built compiled version of the changes

--- source/AliaSQL.Core/Properties/AssemblyInfo.cs
+++ source/AliaSQL.Core/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8aaf5380-94ba-45b9-9681-932b880e8ce9")]
-[assembly: AssemblyVersion("1.4.2.0317")]
-[assembly: AssemblyFileVersion("1.4.2.0317")]
+[assembly: AssemblyVersion("1.4.2.0716")]
+[assembly: AssemblyFileVersion("1.4.2.0716")]
