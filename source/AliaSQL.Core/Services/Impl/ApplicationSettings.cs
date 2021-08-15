24b9dab04cb80c442f5229526a95504174a909f6
ricodp <pithouse@hotmail.com>
2021-08-15 16:36:11 +0800

    allow transcation to look for timeout setting in app.config, if not present, revert to previous/default behaviour

---
+++ source/AliaSQL.Core/Services/Impl/ApplicationSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+
+namespace AliaSQL.Core.Services.Impl
+{
+    internal class ApplicationSettings : IApplicationSettings
+    {
+        public TimeSpan? Timeout()
+        {
+            var timeoutValue = ConfigurationManager.AppSettings["timeout"];
+
+            if (string.IsNullOrEmpty(timeoutValue))
+            {
+                return null;
+            }
+
+            if (!TimeSpan.TryParse(timeoutValue, out var time))
+            {
+                return null;
+            }
+
+            return time;
+        }
+    }
+}
---
+++ source/AliaSQL.Core/Services/Impl/TransactionProvider.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Transactions;
+
+namespace AliaSQL.Core.Services.Impl
+{
+    internal class TransactionProvider
+    {
+        private readonly IApplicationSettings _applicationSettings;
+
+        public TransactionProvider()
+        {
+            _applicationSettings = new ApplicationSettings();
+        }
+        
+        private void SetTransactionManagerField(string fieldName, object value)
+        {
+            typeof(TransactionManager).GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Static)?.SetValue(null, value);
+        }
+
+        public TransactionScope CreateTransactionScope()
+        {
+            var timeout = _applicationSettings.Timeout();
+
+            if (timeout == null)
+            {
+                return new TransactionScope();
+            }
+
+            SetTransactionManagerField("_cachedMaxTimeout", true);
+            SetTransactionManagerField("_maximumTimeout", timeout.GetValueOrDefault());
+            return new TransactionScope(TransactionScopeOption.RequiresNew, timeout.GetValueOrDefault());
+        }
+    }
+}
